19a1bc3788e1f1e41790b85f8bc8b54b9ab948c0
ivddorrka <ivddorrka@gmail.com>
2021-03-03 17:50:01 +0200

    Modified

--- menu.py
+++ menu.py
@@ -5,7 +5,9 @@ import sys
 from notebook import Notebook, Note
 
 class Menu:
+    """To work with menu"""
     def __init__(self):
+        """Init func"""
         self.notebook = Notebook()
         self.choices = {
         "1": self.show_notes,
@@ -15,6 +17,7 @@ class Menu:
         "5": self.quit
         }
     def display_menu(self):
+        """To show menu"""
         print("""
         Notebook Menu
         1. Show all Notes
@@ -24,6 +27,7 @@ class Menu:
         5. Quit
         """)
     def run(self):
+        """To run this file as long as user uses it"""
         while True:
             self.display_menu()
             choice = input("Enter an option: ")
@@ -33,20 +37,24 @@ class Menu:
             else:
                 print("{0} is not a valid choice".format(choice))
     def show_notes(self, notes=None):
+        """To check which notes you already have"""
         if not notes:
             notes = self.notebook.notes
         for note in notes:
             print("{0}: {1}\n{2}".format(
             note.id, note.tags, note.memo))
     def search_notes(self):
+        """To search for key words in all your notes"""
         filter = input("Search for: ")
         notes = self.notebook.search(filter)
         self.show_notes(notes)
     def add_note(self):
+        """To add new note"""
         memo = input("Enter a memo: ")
         self.notebook.new_note(memo)
         print("Your note has been added.")
     def modify_note(self):
+        """To mdify notes"""
         id = input("Enter a note id: ")
         memo = input("Enter a memo: ")
         tags = input("Enter tags: ")
@@ -55,6 +63,7 @@ class Menu:
         if tags:
             self.notebook.modify_tags(id, tags)
     def quit(self):
+        """To stop runing this project"""
         print("Thank you for using your notebook today.")
         sys.exit(0)
 
--- notebook.py
+++ notebook.py
@@ -25,11 +25,11 @@ class Notebook:
         self.notes = []
     def new_note(self, memo, tags=''):
         self.notes.append(Note(memo, tags))
-    def modify_memo(self, note_id, memo):
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+    # def modify_memo(self, note_id, memo):
+    #     for note in self.notes:
+    #         if note.id == note_id:
+    #             note.memo = memo
+    #             break
     def modify_tags(self, note_id, tags):
         for note in self.notes:
             if note.id == note_id:
